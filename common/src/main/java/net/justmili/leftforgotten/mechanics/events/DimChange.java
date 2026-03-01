4ad676367420a5f940252d7a814dcd2bc18b0b13
JustMili <rodx160@gmail.com>
2026-03-01 10:28:33 +0100

    finish removing debug stuff

--- common/src/main/java/net/justmili/leftforgotten/mechanics/events/DimChange.java
+++ common/src/main/java/net/justmili/leftforgotten/mechanics/events/DimChange.java
@@ -1,7 +1,6 @@
 package net.justmili.leftforgotten.mechanics.events;
 
 import dev.architectury.event.EventResult;
-import net.justmili.leftforgotten.LeftForgotten;
 import net.justmili.leftforgotten.init.LFResources;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
@@ -15,22 +14,12 @@ import net.minecraft.world.level.Level;
 public class DimChange {
     public static EventResult attemptDimEntry(LivingEntity entity, DamageSource source, float v) {
         if (!(entity instanceof ServerPlayer player)) return EventResult.pass();
-        if (source == null) {
-            LeftForgotten.LOGGER.warn("source is null!");
-            return EventResult.pass();
-        }
+        if (source == null) return EventResult.pass();
         if (!player.level().dimension().equals(Level.OVERWORLD)) return EventResult.pass();
-        if (!source.is(DamageTypes.FELL_OUT_OF_WORLD)) {
-            LeftForgotten.LOGGER.warn("damage source is not void!");
-            return EventResult.pass();
-        }
-        LeftForgotten.LOGGER.warn("damage source is void!");
+        if (!source.is(DamageTypes.FELL_OUT_OF_WORLD)) return EventResult.pass();
 
         ServerLevel newLevel = player.getServer().getLevel(LFResources.getLevels.ALPHA_MINECRAFT);
-        if (newLevel == null) {
-            LeftForgotten.LOGGER.warn("newLevel is null!");
-            return EventResult.pass();
-        }
+        if (newLevel == null) return EventResult.pass();
 
         player.setHealth(player.getMaxHealth());
         player.teleportTo(newLevel, player.getX(), 150, player.getZ(), player.getYRot(), 90f);
