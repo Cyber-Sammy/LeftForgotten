00f7fa5463078d78f129930c83cafaadf6191866
BluSpring <bluspringindohq@gmail.com>
2026-03-06 06:20:32 +0800

    forgot one

--- fabric/src/main/java/net/justmili/leftforgotten/client/fabric/FabricClient.java
+++ fabric/src/main/java/net/justmili/leftforgotten/client/fabric/FabricClient.java
@@ -1,6 +1,5 @@
 package net.justmili.leftforgotten.client.fabric;
 
-import dev.architectury.registry.registries.RegistrySupplier;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.justmili.leftforgotten.client.CommonClient;
@@ -11,7 +10,7 @@ import net.minecraft.world.level.block.Block;
 public final class FabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        for (RegistrySupplier<Block> block : LFResources.getBlocks()) {
+        for (Block block : LFResources.getBlocks()) {
             BlockRenderLayerMap.INSTANCE.putBlock(block, RenderType.cutout());
         }
         CommonClient.register();
