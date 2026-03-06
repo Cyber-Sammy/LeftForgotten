9a6d77149619b515e9a53c51c3d59ec83952dd94
JustMili <rodx160@gmail.com>
2026-03-06 18:16:58 +0100

    comment redo

--- common/src/main/java/net/justmili/leftforgotten/datagen/LFModelProvider.java
+++ common/src/main/java/net/justmili/leftforgotten/datagen/LFModelProvider.java
@@ -6,17 +6,13 @@ import net.minecraft.data.PackOutput;
 import net.minecraft.data.models.BlockModelGenerators;
 import net.minecraft.data.models.ItemModelGenerators;
 import net.minecraft.data.models.model.ModelTemplates;
-import net.minecraft.data.models.model.TextureMapping;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.properties.Property;
-import net.minecraft.data.models.BlockModelGenerators.TintState;
 
 public class LFModelProvider extends ImprovedModelProvider {
     public LFModelProvider(PackOutput output) {
         super(output);
     }
-
 
     @Override
     public void generateBlockStateModels(BlockModelGenerators blockGen) {
@@ -29,52 +25,34 @@ public class LFModelProvider extends ImprovedModelProvider {
         createFlatItem(itemGen, LFItems.CLAY_BALL.get());
     }
 
-    // To-do helper methods:
-    //      Blocks
-    // "createWoodFamily" - Planks -> Stairs, Slab, Fence, Fence Gate, Door, Trapdoor
-    // "createStoneFamily" - Stone -> Stairs, Slab, Wall (toggle with bool cuz normal stone doesn't have it)
-    // "createRedstoneFamily" - Planks/Stone -> Pressure Plate, Button
-    // "createCubeAll" - Basic cube (parent: block/cube_all)
-    // "createCube" - Basic cube but can put specific textures on a block face (parent: block/cube) + Rotation mode
-    //                (No rot., S/W/N/E y-axis, D/U/N/S/W/E, log XYZ)
-    // "createPlant" - Low flowers etc
-    // "createTallPlant" - Tall flowers
-    // "createCrossPlant" - I don't even know anymore
-    //       Items
-    // "createFlatItem" - Basic flat item
-    // "createFlatBlockItem" - overwrites item model of blocks to be a flat item
-
     // Helper methods
     public static void createWoodFamily(BlockModelGenerators blockGen, Block block) {
-
+        // Creates stairs, slabs, fences, fence gates, doors, trapdoors FROM planks
     }
     public static void createStoneFamily(BlockModelGenerators blockGen, Block block) {
-
+        // Creates stairs, slabs and OPTIONALLY walls from a stone(-like) block
     }
     public static void createRedstoneFamily(BlockModelGenerators blockGen, Block block) {
-
+        // Creates pressure plates and buttons
+        // Both having the option to be disabled but one always has to exist in this method
     }
     public static void createCubeAll(BlockModelGenerators blockGen, Block block) {
-        blockGen.createTrivialCube(block);
+        // Creates a basic all-faces one-texture cube block
     }
     public static void createCube(BlockModelGenerators blockGen, Block block) {
+        // Creates a basic cube block with option for per-face textures and rotation.
+        // Rotation options: No rot., S/W/N/E y-axis, D/U/N/S/W/E, log XYZ
 
+        // Note - Also make: methods for just generating blockstates for rotational blocks with custom models
     }
-    /// What the fuck is TintState????? Why is it needed??
-    public static void createPlant(BlockModelGenerators blockGen, Block block, Block pottedBlock, TintState tintState) {
-        blockGen.createPlant();
+    public static void createPlant(BlockModelGenerators blockGen, Block block) {
+        // For stuff like plants like flowers, saplings, grass
     }
-    public static void createTallPlant(BlockModelGenerators blockGen, Block block, Block pottedPlantBlock, TintState tintState) {
-        blockGen.createDoublePlant(block, tintState);
+    public static void createTallPlant(BlockModelGenerators blockGen, Block block) {
+        // For stuff like tall grass, tall ferns
     }
-    public static void createCrossPlant(BlockModelGenerators blockGen, Block block, TintState tintState) {
-        blockGen.createCrossBlock(block, tintState);
-    }
-    public static void createCrossPlant(BlockModelGenerators blockGen, Block block, TintState tintState, TextureMapping txtMapping) {
-        blockGen.createCrossBlock(block, tintState, txtMapping);
-    }
-    public static void createCrossPlant(BlockModelGenerators blockGen, Block block, TintState tintState, TextureMapping txtMapping, Property<Integer> property, int... propertyValues) {
-        blockGen.createCrossBlock(block, tintState, txtMapping, property, propertyValues);
+    public static void createCrop(BlockModelGenerators blockGen, Block block) {
+        // For crops like carrots, potatoes and so on
     }
 
     public static void createFlatItem(ItemModelGenerators itemGen, Item item) {
