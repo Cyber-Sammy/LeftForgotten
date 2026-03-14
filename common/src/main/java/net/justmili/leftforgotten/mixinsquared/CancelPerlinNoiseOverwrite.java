ced6c5dbb37d29bcd524bea0b8877c64c89b8e77
JustMili <rodx160@gmail.com>
2026-03-15 00:31:01 +0100

    start doing stuff with structures

--- common/src/main/java/net/justmili/leftforgotten/mixinsquared/CancelPerlinNoiseOverwrite.java
+++ common/src/main/java/net/justmili/leftforgotten/mixinsquared/CancelPerlinNoiseOverwrite.java
@@ -1,7 +1,6 @@
 package net.justmili.leftforgotten.mixinsquared;
 
 import com.bawnorton.mixinsquared.api.MixinCanceller;
-import net.justmili.leftforgotten.LeftForgotten;
 
 import java.util.List;
 
@@ -10,10 +9,8 @@ public class CancelPerlinNoiseOverwrite implements MixinCanceller {
     public boolean shouldCancel(List<String> list, String mixinClassName) {
         if (mixinClassName.equals("com.ishland.c2me.opts.math.mixin.MixinOctavePerlinNoiseSampler") ||
             mixinClassName.equals("com.ishland.c2me.opts.math.mixin.MixinPerlinNoiseSampler")) {
-            LeftForgotten.LOGGER.info("Logged MixinSquared");
             return true;
         }
-        LeftForgotten.LOGGER.info("Couldn't Log MixinSquared");
         return false;
     }
 }
