945230658757d7f11277496bcff011ae7d4a833a
JustMili <rodx160@gmail.com>
2026-03-15 21:51:38 +0100

    Set up Forge mixins just because and finish blockly lighting

--- common/src/main/java/net/justmili/leftforgotten/mixin/BlockyLighting.java
+++ common/src/main/java/net/justmili/leftforgotten/mixin/BlockyLighting.java
@@ -1,24 +1,20 @@
 package net.justmili.leftforgotten.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.justmili.leftforgotten.LeftForgotten;
 import net.justmili.leftforgotten.init.LFResources;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.block.ModelBlockRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ModelBlockRenderer.class)
+// Works on Forge and Fabric, but because Forge does mixins in a weird way it doesn't work in Forge Dev Environment
+@Mixin(Minecraft.class)
 public abstract class BlockyLighting {
-
-    @WrapOperation(method = "tesselateBlock", at = @At(value = "INVOKE",
-        target = "Lnet/minecraft/client/Minecraft;useAmbientOcclusion()Z"))
-    private boolean blockyLighting(Operation<Boolean> original) {
+    @Inject(method = "useAmbientOcclusion", at = @At("HEAD"), cancellable = true)
+    private static void blockyLighting(CallbackInfoReturnable<Boolean> cir) {
         Minecraft mc = Minecraft.getInstance();
-        if (mc.player != null && mc.player.level().dimension().equals(LFResources.Levels.ALPHA_MINECRAFT)) {
-            return false;
+        if (mc.level != null && mc.level.dimension().equals(LFResources.Levels.ALPHA_MINECRAFT)) {
+            cir.setReturnValue(false);
         }
-        return original.call();
     }
 }
