df9826d4fc01d1e461e8c1f3d8bdc97229b08b37
JustMili <rodx160@gmail.com>
2026-03-22 16:13:17 +0100

    going insane because forge runData keeps failing so I just copied the jsons from fabric

--- common/src/main/java/net/justmili/leftforgotten/mixin/ReshapeChests.java
+++ common/src/main/java/net/justmili/leftforgotten/mixin/ReshapeChests.java
@@ -12,13 +12,14 @@ import net.minecraft.world.level.block.state.properties.ChestType;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ChestBlock.class)
 public class ReshapeChests {
-    private static final VoxelShape FULL_BLOCK = Block.box(0, 0, 0, 16, 16, 16);
+    @Unique private static final VoxelShape FULL_BLOCK = Block.box(0, 0, 0, 16, 16, 16);
 
     @Inject(method = "getShape", at = @At("HEAD"), cancellable = true)
     private void lf$alphaChestShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context,
