ff0b2b464eb7d44495bd1ffa297ce3ef694ab9fb
BluSpring <bluspringindohq@gmail.com>
2026-03-23 05:18:37 +0800

    fixed chests not being visible as an item on Fabric

--- fabric/src/main/java/net/justmili/leftforgotten/client/fabric/FabricClient.java
+++ fabric/src/main/java/net/justmili/leftforgotten/client/fabric/FabricClient.java
@@ -1,15 +1,18 @@
 package net.justmili.leftforgotten.client.fabric;
 
 import dev.architectury.registry.client.level.entity.EntityRendererRegistry;
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
 import net.justmili.leftforgotten.client.CommonClient;
 import net.justmili.leftforgotten.entity.renderer.LFBoatRenderer;
 import net.justmili.leftforgotten.init.LFEntities;
 import net.justmili.leftforgotten.init.LFResources;
+
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.world.level.block.Block;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
 
 public final class FabricClient implements ClientModInitializer {
     @Override
@@ -23,7 +26,13 @@ public final class FabricClient implements ClientModInitializer {
         CommonClient.register();
 
         ModelLoadingPlugin.register(context -> {
-            context.modifyModelAfterBake().register((model, ctx) -> new ClassicBlocksModel(model));
+            context.modifyModelAfterBake().register((model, ctx) -> {
+                if (ctx.id() instanceof ModelResourceLocation resourceLocation && ctx.id().getNamespace().equals("minecraft") && !resourceLocation.getVariant().equals("inventory") && (ctx.id().getPath().equals("furnace") || ctx.id().getPath().equals("crafting_table"))) {
+                    return new ClassicBlocksModel(model);
+                }
+
+                return model;
+            });
         });
     }
 }
